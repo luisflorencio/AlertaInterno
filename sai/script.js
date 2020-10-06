045ad651b32a68d58fe6cde2c2d504823d669394
Luís Florêncio <59532805+luisflorencio@users.noreply.github.com>
2020-10-06 22:36:36 +0100

    Add files via upload

---
+++ sai/script.js
@@ -0,0 +1,39 @@
+      function play1() {
+        var audio = document.getElementById("audio1");
+        if (audio.paused) {
+          audio.play();
+        } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+      }
+
+      function play2() {
+        var audio = document.getElementById("audio2");
+        if (audio.paused) {
+          audio.play();
+        } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+      }
+
+      function play3() {
+        var audio = document.getElementById("audio3");
+        if (audio.paused) {
+          audio.play();
+        } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+      }
+
+      function play4() {
+        var audio = document.getElementById("audio4");
+        if (audio.paused) {
+          audio.play();
+        } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+      }
