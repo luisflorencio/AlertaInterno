ee0dd72f1cba28cb746e5adc071f9009869ede79
Luís Florêncio <59532805+luisflorencio@users.noreply.github.com>
2020-10-06 23:50:11 +0100

    Add files via upload

--- sai/script.js
+++ sai/script.js
@@ -1,39 +1,44 @@
-      function play1() {
-        var audio = document.getElementById("audio1");
-        if (audio.paused) {
-          audio.play();
-        } else {
-          audio.pause();
-          audio.currentTime = 0;
-      }
-      }
+function play1() {
+var audio = document.getElementById("audio1");
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+    audio.currentTime = 0;
+}
+}
 
-      function play2() {
-        var audio = document.getElementById("audio2");
-        if (audio.paused) {
-          audio.play();
-        } else {
-          audio.pause();
-          audio.currentTime = 0;
-      }
-      }
+function play2() {
+var audio = document.getElementById("audio2");
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+    audio.currentTime = 0;
+}
+}
 
-      function play3() {
-        var audio = document.getElementById("audio3");
-        if (audio.paused) {
-          audio.play();
-        } else {
-          audio.pause();
-          audio.currentTime = 0;
-      }
-      }
+function play3() {
+var audio = document.getElementById("audio3");
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+    audio.currentTime = 0;
+}
+}
 
-      function play4() {
-        var audio = document.getElementById("audio4");
-        if (audio.paused) {
-          audio.play();
-        } else {
-          audio.pause();
-          audio.currentTime = 0;
-      }
-      }
+function play4() {
+var audio = document.getElementById("audio4");
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+    audio.currentTime = 0;
+}
+}
+
+/*var teste = window.prompt('QUAL ')
+window.alert(teste + ' tá bem tá')
+
+parseFloat()*/
